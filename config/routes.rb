f787bbf26e5bdc0d4d20949b030b3eaf69419711
wakaba ohahsi <wakaba@wireless-fs-0-165.kyoto-wu.ac.jp>
2015-07-02 13:44:58 +0900

    カートを格好良くした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :line_items
-  resources :line_items
-  get 'store/index'
+  resources :carts
+  get 'store/index', :as => 'store'
 
   resources :carts
   resources :line_items
